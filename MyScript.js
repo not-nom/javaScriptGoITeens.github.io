182b9241f440a360490895c42c4dd823c1e0df7e
not-nom <nominated200@gmail.com>
2021-10-24 19:28:51 +0300

    Added modifications

---
+++ MyScript.js
@@ -0,0 +1,10 @@
+let form = document.querySelector("#formular")
+function hello(){
+    document.body.classList.toggle('show-nav');
+}
+function forma() {
+    form.classList.add("displalist");
+}
+function closeWindow() {
+    form.classList.remove("displalist");
+}
